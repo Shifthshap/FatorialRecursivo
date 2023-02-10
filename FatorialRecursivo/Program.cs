5e124167c5d46bd152ab4d05b1728212b7995d0e
Jardel Mandel Cardoso <jardel.mandel@gmail.com>
2023-02-10 16:36:35 -0300

    Add project files.

---
+++ FatorialRecursivo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace FatorialRecursivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o numero para calcular o fatorial: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("O resultado da fatorial de " + num + " é " + Fatorial(num));
+            Console.ReadKey();
+        }
+
+        static int Fatorial(int num)
+        {
+            if( num <=0 )
+            {
+                return 1;
+            }
+            
+            return num * Fatorial(num - 1);
+        }
+
+        
+    }
+}
